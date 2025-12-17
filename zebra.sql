0092ff6be56adbdd77a02e0d00c2cc58d35d43f7
Amanda Szampias <ASzampiasSWD@gmail.com>
2025-12-17 06:11:52 -0500

    changes

--- zebra.sql
+++ zebra.sql
@@ -20,8 +20,6 @@ CREATE TABLE users(
  created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP
 );
 
-ALTER TABLE users ADD COLUMN is_active BOOLEAN DEFAULT TRUE;
-
 CREATE TABLE printer_types(
  printer_type_id varchar(50) PRIMARY KEY,
  printer_manufacturer varchar(50) NOT NULL,
@@ -31,7 +29,7 @@ CREATE TABLE printer_types(
 
 CREATE TABLE printers(
  serial_number_id varchar(50) PRIMARY KEY,
- printer_type varchar(100) REFERENCES printer_type(printer_type_id),
+ printer_type varchar(100) REFERENCES printer_types(printer_type_id),
  times_worked_on integer DEFAULT 0,
  money_spent_on_repairs numeric(10, 2) DEFAULT 0.00,
  created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP
@@ -40,7 +38,7 @@ CREATE TABLE printers(
 CREATE TABLE printer_parts(
  printer_part_id varchar(50) PRIMARY KEY,
  printer_part_name varchar(100) NOT NULL, 
- printer_type varchar(50) REFERENCES printer_type(printer_type_id),
+ printer_type varchar(50) REFERENCES printer_types(printer_type_id),
  current_cost numeric(10, 2) DEFAULT 0.00,
  total_printer_parts integer DEFAULT 0,
  created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
@@ -55,16 +53,16 @@ CREATE TABLE issues(
 CREATE TABLE repairs(
  repair_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
  serial_number_id varchar(50) REFERENCES printers(serial_number_id) NOT NULL,
- printer_type varchar(100) REFERENCES printer_type(printer_type_id) NOT NULL,
+ printer_type varchar(100) REFERENCES printer_types(printer_type_id) NOT NULL,
  user_id varchar(100) REFERENCES users(user_id) NOT NULL,
  assist_id varchar(100) REFERENCES users(user_id) DEFAULT NULL,
  printer_part_id varchar(100) REFERENCES printer_parts(printer_part_id) DEFAULT NULL,
  printer_location varchar(100) NOT NULL,
  station_number varchar(50) NULL, 
  time_worked_on integer NOT NULL,
- issue_id varchar(100) REFERENCES issues(issue_id) NOT NULL,
+ issue_id integer REFERENCES issues(issue_id) NOT NULL,
  money_saved numeric(10, 2) DEFAULT 0.00, 
- date_time_fixed timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+ date_time_fixed timestamp with time zone DEFAULT CURRENT_TIMESTAMP
 );
 
 
@@ -72,7 +70,11 @@ INSERT INTO organizations VALUES ('ORF3', '2020 Northgate Commerce Pkwy', 'Suffo
 INSERT INTO organizations VALUES ('ORF2', '5045 Portsmouth Blvd', 'Chesapeake', 'VA', '23321', 'USA');
 INSERT INTO organizations VALUES ('ORF4', '1795 Dam Neck Rd', 'Virginia Beach', 'VA', '23453', 'USA');
 INSERT INTO users VALUES ('szampiam', 'ORF3', 'Amanda', 'Szampias');
-INSERT INTO printer_type VALUES ('ZT411', 'Zebra');
+INSERT INTO printer_types VALUES ('ZT411', 'Zebra');
+INSERT INTO ISSUES VALUES (DEFAULT, 'Dirty Roller');
+INSERT INTO ISSUES VALUES (DEFAULT, 'Broken Media Rewind Spindle');
+INSERT INTO ISSUES VALUES (DEFAULT, 'Configuration Issue');
+INSERT INTO ISSUES VALUES (DEFAULT, 'Calibration Issue');
 INSERT INTO printer_parts VALUES ('P1058930-058', 'Media Rewind Spindle', 'ZT411', '263.55', 1);
 INSERT INTO printers VALUES ('ZT92022290', 'ZT411');
-INSERT INTO repairs VALUES ('ZT92022290', 'ZT411', 'szampiam', NULL, 'P1058930-058', 'IT Cage', NULL, 45, 263.55);
+INSERT INTO repairs VALUES (DEFAULT, 'ZT92022290', 'ZT411', 'szampiam', NULL, 'P1058930-058', 'IT Cage', NULL, 45, 1, 263.55);
